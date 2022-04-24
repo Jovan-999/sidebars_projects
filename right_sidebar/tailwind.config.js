baf83dd2fa638c87393fc5a50bd7f96707627cd7
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-04-24 19:53:28 +0200

    right_sidebar

---
+++ right_sidebar/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Poppins'],
+      },
+      colors: {
+        primary: '#009cff',
+      },
+      width: {
+        inherit: 'inherit',
+      },
+      height: {
+        inherit: 'inherit',
+        500: '500px',
+        32: '3200px',
+        70: '70px',
+      },
+    },
+  },
+  plugins: [],
+};

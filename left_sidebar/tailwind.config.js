644ad2f5eb1d3b56f4423210efd52d3048c4daa8
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-04-24 23:04:56 +0200

    nearly_finished

---
+++ left_sidebar/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Poppins'],
+      },
+      colors: {
+        primary: '#5e5e5e',
+      },
+    },
+  },
+  plugins: [],
+};
